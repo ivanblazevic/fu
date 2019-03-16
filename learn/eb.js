9ba22b941ded8b33a7ef3bc018ddee1f4a188084
bla <zajednicki.projekt1@gmail.com>
2019-03-16 13:32:10 +0100

    Update

--- learn/eb.js
+++ learn/eb.js
@@ -5,7 +5,9 @@ function includeHTML() {
 
   const height = elmnt.getAttribute("data-height") || 400;
 
-  file = "http://localhost:8080/?t=" + file;
+  const path = window.location.hostname.indexOf("localhost") > -1 ? "" : "learn";
+    
+  file = "//" + window.location.hostname + "/" + path + "?t=" + file;
 
   if (file) {
     /*make an HTTP request using the attribute value as the file name:*/
